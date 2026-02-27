3b4e1f5c6d4e31c9d587bea1408b451758e082a1
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-02-27 22:19:41 +0100

    chore: migrate publish settings to `project.scala`

--- project.scala
+++ project.scala
@@ -1,5 +1,7 @@
 //> using scala 3.8.2
+
 //> using test.dep org.scalameta::munit::1.2.3
+
 //> using options -deprecation -feature -new-syntax -unchecked
 //> using options -language:noAutoTupling
 //> using options -Vprofile -Xprint-inline
@@ -9,3 +11,13 @@
 //> using options -Yshow-suppressed-errors -Yshow-var-bounds
 //> using options -Wsafe-init -Werror -Wunused:all
 ////> using options -Yprofile-enabled" -Yprofile-trace:debug/compile-trace.json"
+
+//> using publish.organization io.github.halotukozak
+//> using publish.name made
+//> using publish.computeVersion git:tag
+//> using publish.description "M&DE - Mirror Annotations & Default Extraction"
+//> using publish.url https://github.com/halotukozak/made
+//> using publish.license MIT
+//> using publish.vcs github:halotukozak/made
+//> using publish.repository central
+//> using publish.developer "halotukozak|Bartłomiej Kozak|https://github.com/halotukozak"
