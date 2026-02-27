19f034fe55592726c04f5ad8e53f82221e430b7b
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-02-27 19:08:07 +0100

    Move publish configuration from `project.scala` to `.publish/publish.scala` for better separation of concerns

---
+++ .publish/publish.scala
@@ -0,0 +1,9 @@
+//> using publish.organization io.github.halotukozak
+//> using publish.name made
+//> using publish.computeVersion git:tag
+//> using publish.description "M&DE - Mirror Annotations & Default Extraction"
+//> using publish.url https://github.com/halotukozak/made
+//> using publish.license MIT
+//> using publish.vcs github:halotukozak/made
+//> using publish.repository central
+//> using publish.developer "halotukozak|Bartłomiej Kozak|https://github.com/halotukozak"
--- project.scala
+++ project.scala
@@ -1,16 +1,5 @@
 //> using scala 3.8.2
 //> using test.dep org.scalameta::munit::1.2.3
-
-//> using publish.name made
-//> using publish.organization io.github.halotukozak
-//> using publish.computeVersion git:tag
-//> using publish.description "Mirror Annotations & Default Extraction"
-//> using publish.url https://github.com/halotukozak/made
-//> using publish.license MIT
-//> using publish.vcs github:halotukozak/made
-//> using publish.developer "halotukozak|Bartłomiej Kozak|https://github.com/halotukozak"
-//> using publish.repository central
-
 //> using options -deprecation -feature -new-syntax -unchecked
 //> using options -language:noAutoTupling
 //> using options -Vprofile -Xprint-inline
